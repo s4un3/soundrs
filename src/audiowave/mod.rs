025b21227523df92bba87fc803a052d645ecd508
Pedro H. Sauné <pedrohsaune@gmail.com>
2024-08-08 12:06:01 -0300

    partially implement `new`

--- src/audiowave/mod.rs
+++ src/audiowave/mod.rs
@@ -1,5 +1,7 @@
 mod utils;
 
+use std::vec;
+
 use crate::function::Function;
 use num::Float;
 use utils::scale_wave;
@@ -12,7 +14,11 @@ struct AudioWave<N: Float, M: Float> {
     wave: Vec<N>,
 }
 
-impl<N: Float, M: Float> AudioWave<N, M> {
+impl<N: Float, M: Float> AudioWave<N, M>
+where
+    usize: From<M>,
+    M: From<u32>,
+{
     fn new(
         freq: Function<N>,
         amp: Function<N>,
@@ -20,24 +26,29 @@ impl<N: Float, M: Float> AudioWave<N, M> {
         latency: M,
         samplerate: u32,
         waveform: Function<N>,
-        yclip: N,
-    ) -> AudioWave<N, M> {
+        yclip: N
+    ) -> Option<AudioWave<N, M>> {
+        let f_samplerate: M = samplerate.into();
+        let computed_capacity = f_samplerate * duration;
+        let veccapacity: usize = usize::from(computed_capacity);
+        let mut vec: Vec<N> = Vec::with_capacity(veccapacity);
         todo!()
     }
 
     fn add(self, other: AudioWave<N, M>) -> Option<AudioWave<N, M>> {
-        if self.samplerate != other.samplerate {return None}
+        if self.samplerate != other.samplerate {
+            return None;
+        }
         let significance = self.significance + other.significance;
-        let wave = utils::scale_wave(
-            utils::sum_waves(
-                utils::scale_wave(self.wave, self.significance),
-                scale_wave(other.wave, other.significance),
-            ),
-            N::from(1.0).expect("1.0 should be a valid float.") / significance,
-        );
+        let wave = utils::sum_waves(self.wave, other.wave);
         let duration = self.duration.max(other.duration);
         let samplerate = self.samplerate;
-        Some(AudioWave{ significance, samplerate, duration, wave })
+        Some(AudioWave {
+            significance,
+            samplerate,
+            duration,
+            wave,
+        })
     }
 
     fn append(self, other: AudioWave<N, M>) -> Option<AudioWave<N, M>> {
