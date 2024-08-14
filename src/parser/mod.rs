56e131c3da87d74020d6d2086c11ec9bbcd97282
Pedro H. Sauné <pedrohsaune@gmail.com>
2024-08-14 13:46:26 -0300

    Changes on 'note_to_semitone'

--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -8,21 +8,12 @@ pub enum Semitone {
     Rest,
 }
 
-fn remove_last(s: &str) -> &str {
-    let mut s = s.chars();
-    s.next_back();
-    s.as_str()
-}
-
-fn get_last(s: &str) -> Option<char> {
-    let mut s = s.chars();
-    s.next_back()
-}
-
 pub fn note_to_semitone(note: &str, default_octave: Option<u8>) -> Option<Semitone> {
     if note == "_" {
         Some(Semitone::Rest);
     };
+
+    let default_octave = default_octave.unwrap_or(4);
     let mut s: Float = 0.0;
 
     let mut note = note.chars();
@@ -40,16 +31,16 @@ pub fn note_to_semitone(note: &str, default_octave: Option<u8>) -> Option<Semito
 
     let modifiers = note.as_str();
     if !modifiers.is_empty() {
-
         let mut i: usize = 0;
         let mut state: u8 = 0; // 0 - sharps and flats
         let mut tempstring = "".to_owned();
+        let mut isoctaverelative = false;
         loop {
             let current_char: char;
             let extracting_char = modifiers.chars().nth(i);
-            match extracting_char{
-                Some(c) => current_char=c,
-                None => {break},
+            match extracting_char {
+                Some(c) => current_char = c,
+                None => break,
             }
             i += 1;
             if state == 0 {
@@ -59,12 +50,25 @@ pub fn note_to_semitone(note: &str, default_octave: Option<u8>) -> Option<Semito
                     ')' => return None,
                     '(' => state = 1, // 1 - microtones
                     '+' => {
-                        state = 2; // 2 - octave adjustmnents
+                        isoctaverelative = true;
+                        state = 2; // 2 - octave adjustments
                         s += 12.0;
                     }
                     '-' => {
+                        isoctaverelative = true;
                         state = 2;
                         s -= 12.0;
+                    }
+                    '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' => {
+                        if isoctaverelative {
+                            return None;
+                        }
+                        let val: Result<u8, _> = current_char.to_string().parse();
+                        match val {
+                            Ok(v) => s += 12.0 * (v - default_octave) as Float,
+                            Err(_) => (),
+                        }
+                        break;
                     }
                     _ => return None,
                 };
@@ -72,15 +76,27 @@ pub fn note_to_semitone(note: &str, default_octave: Option<u8>) -> Option<Semito
                 match current_char {
                     'c' => {
                         match tempstring.parse::<Float>() {
-                            Ok(value) => s += value/100.0,
+                            Ok(value) => s += value / 100.0,
                             Err(_) => return None,
                         };
+                        tempstring = "".to_string();
                     }
                     ')' => state = 2,
                     _ => tempstring.push(current_char),
                 }
             } else if state == 2 {
                 match current_char {
+                    '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' => {
+                        if isoctaverelative {
+                            return None;
+                        }
+                        let val: Result<u8, _> = current_char.to_string().parse();
+                        match val {
+                            Ok(v) => s += 12.0 * (v - default_octave) as Float,
+                            Err(_) => (),
+                        }
+                        break;
+                    }
                     '+' => s += 12.0,
                     '-' => s -= 12.0,
                     _ => break,
